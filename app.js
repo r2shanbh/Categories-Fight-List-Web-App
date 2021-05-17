8c551a880778d2e8557dc2db1c9c296413b441c9
Raksha Shanbhag <r2shanbh@gmail.com>
2021-05-17 18:41:34 -0400

    Fight List updates

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
-const _ = require("lodash");
 
 const app = express();
 
@@ -13,29 +12,36 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin:Rakshu11$@cluster0.eez5d.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+mongoose.connect("mongodb+srv://admin:testtest@cluster0.eez5d.mongodb.net/fightListDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify : true});
 
 const today = date.getDate();
 
+const gameSchema = {
+  category : String,
+  code : String
+}
+
+const Game = mongoose.model("Game", gameSchema);
+
 const itemsSchema = {
-  item : String
+  item  : String
 }
 
 const Item = mongoose.model("Item", itemsSchema);
 
-const listsSchema = {
-  name : String,
+const playerBoardSchema = {
+  gameCode : String,
+  player: String,
   items : [itemsSchema]
 };
 
-const List = mongoose.model("Lists", listsSchema);
+const Board = mongoose.model("Board", playerBoardSchema);
 
-const welcome = new Item ({ item : "The to-do list App"})
-const plusButton = new Item ({item : "Hit + button to add new item"})
-const unCheck = new Item ({item : "<-- Click on this box to uncheck an item"})
+const unCheck = new Item ({item : "<-- Click on this box to remove an item"})
 
-const defaultItems = [welcome, plusButton, unCheck];
+const defaultItems = [unCheck];
 
+/*
 app.get("/", function(req, res) {
   Item.find({}, function(err, foundItems){
     if(err){
@@ -56,56 +62,118 @@ app.get("/", function(req, res) {
   });
 
 });
+*/
+
+app.get("/", function(req,res){
+    res.render("main");
+});
+
+app.post("/newGame", function(req, res){
+  const newCategory = req.body.newGameCategory;
+  const random = Math.floor(Math.random() * 234);
+  const newCode = "FightList_"+ newCategory.substr(1,4) +"_"+random;
+
+  const dbItem = new Game({
+    category : newCategory,
+    code : newCode
+  });
+  dbItem.save();
+  res.redirect("/");
+});
+
+app.post("/enterGame", function(req, res){
+  const code= req.body.gameCode;
+  const username = req.body.username;
+
+  console.log(code);
+
+  Game.findOne({code : code} , function(err, game){
+      if(!game){
+        console.log("!Not available");
+        res.redirect("/");
+      }
+      else{
+        Board.findOne({gameCode : code, player: username} , function(err, playerBoard){
+          if(!playerBoard){
+            const dbItem = new Board ({
+              gameCode : code,
+              player : username,
+              items : defaultItems
+            });
+            dbItem.save();
+          }
+
+          res.redirect("/boards/"+code+"/"+username);
+        });
+      }
+  });
+
+});
+
+app.get("/boards/:code/:username", function(req,res){
+  const code = req.params.code;
+  const username = req.params.username;
+
+  Board.findOne({gameCode : code, player: username} , function(err, playerBoard){
+    if(!err){
+      if(!playerBoard){
+        res.redirect("/");
+      }else{
+
+        Game.findOne({code : code}, function(err, game){
+          if(game){
+            const category = game.category;
+            res.render("list", {day: today, listTitle: username, category : category, gameCode : code, newListItems: playerBoard.items})
+          }
+        });
+      }
+
+    }else{
+      console.log(err);
+    }
+  });
+})
 
 app.post("/delete", function(req, res){
   const ID = req.body.checkBox;
-  const listName = req.body.listName;
+  const username = req.body.username;
+  const gameCode = req.body.gameCode;
 
-  if (listName == "Today"){
-    Item.findByIdAndDelete(ID, function(err, docs){
-      if(err){
-        console.log(err);
-      }
-    });
-    res.redirect("/");
-  }else{
-    List.findOneAndUpdate({name: listName}, {$pull : {items : {_id : ID }} }, function(err, foundList){
-      if(!err){
-        res.redirect("/"+listName);
-      }else{
-        console.log(err);
-      }
-    });
-  }
+  Board.findOneAndUpdate({gameCode : gameCode, player : username}, {$pull : {items : {_id : ID }} }, { new: true},function(err, foundBoard){
+    if(!err){
+      console.log("mee");
+      console.log(foundBoard);
+      res.redirect("/boards/"+gameCode+"/"+username);
+    }else{
+      console.log(err);
+    }
+  });
 });
 
 
-app.post("/", function(req, res){
+app.post("/addItem", function(req, res){
   const itemName = req.body.newItem;
-  const listName = req.body.list;
+  const username = req.body.username;
+  const gameCode = req.body.gameCode;
 
   const dbItem = new Item ({
     item : itemName
   });
 
-  if (listName == "Today"){
-    dbItem.save();
-    res.redirect("/");
-  }else{
-    List.findOne({name: listName}, function(err, foundList){
+    Board.findOne({gameCode: gameCode, player: username}, function(err, foundBoard){
       if(!err){
-        foundList.items.push(dbItem);
-        foundList.save();
+        foundBoard.items.push(dbItem);
+        foundBoard.save();
       }else{
         console.log(err);
       }
     });
-    res.redirect("/"+listName);
-  }
+    res.redirect("/boards/"+gameCode+"/"+username);
 });
 
 
-app.get("/:customList", function(req,res){
+/*
+app.get("/boards/listTitle/username", function(req,res){
   const listName = _.capitalize(req.params.customList);
 
   List.findOne({name : listName}, function(err, foundList){
@@ -126,7 +194,7 @@ app.get("/:customList", function(req,res){
     }
   });
 });
-
+*/
 app.get("/about", function(req, res){
   res.render("about");
 });
